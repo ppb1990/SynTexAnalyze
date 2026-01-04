ce24d2123dd75bb632f14b1a1aa21ecf3c103754
zhuoli <zl0401@princeton.edu>
2026-01-03 23:56:57 -0500

    Update the tiff processing file and move some of the helper functions into utils.py

---
+++ SynTexAnalyze/image_processing/tiff_processing.py
@@ -0,0 +1,93 @@
+import os,sys
+import numpy as np
+import pandas as pd
+from numba.core.typing.builtins import Range
+
+from Python_files.current_version.multi_processing import out_dir_lst
+
+from utils import *
+
+class TiffSetProcessor:
+    def __init__(self,
+                 tiff_dir, out_dir, img_ctrl, img_ctrl_dir,
+                 root='C:\\', GS_path='default',
+                 upper_lim=0.015, lower_lim='eq',
+                 wavelength=0.1819,
+                 Li_lp_a=3.507,
+                 file_type='texture', azm_range=None,
+                 no_dark_sub=False, SetName=None,
+                 detector=None
+                 ):
+        # file path for single data set
+
+        # need to modify the "Li_li_p" to another parameter
+        # so it can be used for other structure
+
+        tiff_dir = tiff_dir.rstrip("\\/")  # clean the path
+
+        if not no_dark_sub:
+            tiff_dir = os.path.join(tiff_dir,"dark_sub")
+        self.tiff_dir = tiff_dir
+
+        self.file_lst = [f for f in os.listdir(tiff_dir) if f.lower().endswith((".tif", ".tiff"))]
+        self.file_path = [os.path.join(tiff_dir,f) for f in self.file_lst]
+        self.out_dir = out_dir
+
+        if GS_path == 'default':
+            if sys.platform.startswith("win"):  # for Windows system
+                GS_path = r'C:\XRD\GSAS-II\GSAS-II\GSASII'
+                if not os.path.isdir(GS_path):
+                    raise FileNotFoundError(f"GSAS-II path not found: {GS_path}")
+            else:
+                raise ValueError("Please specify the GSAS-II path")
+        elif GS_path == 'auto':
+            def find_gsasii(root):
+                for dir_path, dir_names, _ in os.walk(root):
+                    if "GSAS-II" in dir_names:
+                        return os.path.join(dir_path, "GSAS-II")
+                raise FileNotFoundError(f"GSAS-II was not detected, please specify your GSAS-II path!!!")
+            GS_path = find_gsasii(root)
+        else:
+            if not os.path.isdir(GS_path):
+                raise FileNotFoundError(f"GSAS-II path not found: {GS_path}")
+
+        self.GS_path = GS_path
+        self.img_ctrl_path = os.path.join(img_ctrl_dir,img_ctrl)
+
+        if detector is None:
+            self.detector = 'PE'
+        elif detector == 'Pilatus':
+            self.detector = 'Pilatus'
+        else:
+            raise ValueError(f"The detector needs to be either PE or Pilatus for now!")
+
+        print(self.tiff_dir)
+        print('The detector is: {}.'.format(self.detector))
+        print(f"The image control file is: {img_ctrl}")
+
+        # temp fix for the file type for now, in the future will make it more universal.
+        # should make a function to handle the data set more properly
+        self.file_type = file_type
+        if file_type == 'texture':
+            if SetName:
+                self.SetName = SetName
+            else:
+                self.SetName = self.tiff_dir.split('texture_')[1].split('_f')[0]
+        elif file_type == 'Texture':  # temp fix for the ONR data
+            self.SetName = self.tiff_dir.split('Texture_')[1].split('_f')[0]
+        elif file_type == 'mapping':
+            self.SetName = self.tiff_dir.split('AF_')[1].split('_mapping')[0]
+        elif file_type == 'Operando':
+            self.SetName = self.tiff_dir.split('\\')[-3]
+
+        self.wavelength = wavelength  # define the wavelength in angstroms
+        self.Li_lp_a = Li_lp_a  # default is lithium and the lithium
+        self.im_tth, self.im_azm = get_intTAmap(self.GS_path, self.file_path, self.img_ctrl_path)
+        self.ring_conditions = get_search_conditions(upper_lim, lower_lim, Li_lp_a, wavelength)
+        if azm_range is not None:  # a list with two numbers, e.g. [90,270]
+            self.azm_range = azm_range
+        else:
+            self.azm_range = None
+        print(f"Initialization complete with wavelength: {self.wavelength}, search conditions: {upper_lim}")
+
+
---
+++ SynTexAnalyze/image_processing/utils.py
@@ -0,0 +1,124 @@
+import sys
+import numpy as np
+
+def d_to_tth(d, w):  # calculate the tth from d spacing
+    tth = np.arcsin(w / (d * 2)) * 2  # results in radians
+    tth = tth * 180 / np.pi
+    return tth
+
+
+def get_rotation_th(file_name):  # for texture sample, get rotation angle
+    rotation_th = file_name.split('th_')[1].split(',')[0]
+    return rotation_th
+
+
+def get_psn_xy(file_name):  # for Austin's sample, get psn_x, psn_y
+    psn_x = file_name.split('sample_x_')[1].split('mm')[0].replace(',', '.')
+    psn_y = file_name.split('sample_y_')[1].split('mm')[0].replace(',', '.')
+    psn_x = float(psn_x)
+    psn_y = float(psn_y)
+    return psn_x, psn_y
+
+
+def get_tth_azm(detector, intTAmap):
+
+    # Currenlty using numbers such as 2048, 1475, 1679 for PE and Pilatus detecors
+    # In the future should use a raw file to determine the shape
+
+    if detector == 'PE':
+
+        im_azm = np.array([[0 for j in range(2048)] for i in range(2048)], dtype=np.float64)
+        im_tth = np.array([[0 for j in range(2048)] for i in range(2048)], dtype=np.float64)
+
+        for i in range(16):
+            for j in range(16):
+                temp_azm_map = intTAmap[i][j][0]
+                temp_tth_map = intTAmap[i][j][1]
+                for x in range(128):
+                    for y in range(128):
+                        out_x = 128 * i + x
+                        out_y = 128 * j + y
+
+                        im_azm[out_x, out_y] = temp_azm_map[x, y]
+                        im_tth[out_x, out_y] = temp_tth_map[x, y]
+    elif detector == 'Pilatus':
+
+        # for Pilatus, the shape of the detector is very different from PE, so need to modify this
+
+        im_azm = np.array([[0 for j in range(1475)] for i in range(1679)], dtype=np.float64)
+        im_tth = np.array([[0 for j in range(1475)] for i in range(1679)], dtype=np.float64)
+
+        for i in range(14):
+            for j in range(12):
+                temp_azm_map = intTAmap[i][j][0]
+                temp_tth_map = intTAmap[i][j][1]
+
+                if i == 13:  # the matrix bending is not uniform, so the last one is 15 for i and 67 for j
+                    range_i = 15
+                else:
+                    range_i = 128
+                if j == 11:
+                    range_j = 67
+                else:
+                    range_j = 128
+
+                for x in range(range_i):
+                    for y in range(range_j):
+                        out_x = 128 * i + x
+                        out_y = 128 * j + y
+
+                        im_azm[out_x, out_y] = temp_azm_map[x, y]
+                        im_tth[out_x, out_y] = temp_tth_map[x, y]
+
+    return im_tth, im_azm
+
+
+def get_intTAmap(GS_path,file_path, img_ctrl_path):
+    sys.path.insert(GS_path)  # needed to "find" GSAS-II modules
+    import GSASIIscriptable as G2sc
+    import gc
+
+    cache = {}
+    file_path = file_path[0]  # temp use
+    # ctrl_path = os.path.join('D://Postdoc//XPD-2024-2//Zhuo','texture-control.imctrl')
+
+    gpx = G2sc.G2Project(newgpx='temp.gpx')  # temp use
+    img = gpx.add_image(file_path, cacheImage=False)  # don't need to cache image
+    img[0].loadControls(img_ctrl_path)
+
+    # cache['intMaskMap'] = img[0].IntMaskMap()
+    cache['intTAmap'] = img[0].IntThetaAzMap()
+
+    im_tth, im_azm = get_tth_azm(cache['intTAmap'])
+
+    del cache, gpx
+
+    return im_tth, im_azm
+
+
+def get_search_conditions(self,
+                              upper_lim,
+                              lower_lim,
+                              Li_lp_a,
+                              wavelength
+                          ):
+    # rings should be like [0] for (110) or [0,1] for (110), (200)
+    # the lim terms are used to determine the upper boundary and lower boundary
+    # currently calculate all three hkls
+    ring_conditions = {'hkl': ['tth', 'tth + uper_lim', 'tth - lower_lim']}
+
+    Li_hkl = ['110', '200', '211']
+    Li_hkl_sum = [2, 4, 6]  # h^2 + k^2 + l^2 for (110) (200) (211)
+    Li_d = [Li_lp_a * np.sqrt(1 / i) for i in Li_hkl_sum]
+    Li_tth = [self.d_to_tth(i, wavelength) for i in Li_d]
+
+    if lower_lim == 'eq':
+        for i, j in zip(Li_hkl, Li_tth):
+            ring_conditions[i] = [j, j + upper_lim, j - upper_lim]
+        return ring_conditions
+    elif type(lower_lim) == float or type(lower_lim) == int:
+        for i, j in zip(Li_hkl, Li_tth):
+            ring_conditions[i] = [j, j + upper_lim, j - lower_lim]
+        return ring_conditions
+    else:
+        raise ValueError(" lower_lim need to be either 'eq', int, or float!!!")
